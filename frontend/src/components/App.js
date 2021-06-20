98d1ffc5c922fc5c130f4a0e54b942cd2728bd9d
Nadia Kotegova <nadinekoteg@gmail.com>
2021-06-20 23:02:56 +0300

    15 спринт likes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -92,12 +92,12 @@ export default function App() {
   }
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card?.likes.some((i) => i._id === currentUser._id);
     api
-      .toggleLike(card._id, isLiked)
+      .toggleLike(card?._id, isLiked)
       .then((newCard) => {
         setCards((state) =>
-          state.map((c) => (c._id === card._id ? newCard : c))
+          state?.map((c) => (c?._id === card?._id ? newCard : c))
         );
       })
       .catch((err) => {
