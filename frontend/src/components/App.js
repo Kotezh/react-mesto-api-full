66db4cfec16c287c169c0e772a15e658488f4807
Nadia Kotegova <nadinekoteg@gmail.com>
2021-07-07 02:05:07 +0300

    fix spinner

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -69,10 +69,10 @@ export default function App() {
       .catch((err) => {
         console.log(err);
       })
-      .finally(() => {
-        console.log('I WANT TO STOP SPINNER!');
-        setIsLoading(false);
-      });
+      // .finally(() => {
+      //   console.log('I WANT TO STOP SPINNER!');
+      //   setIsLoading(false);
+      // });
   }, [history]);
 
   function handleUpdateUser(data) {
