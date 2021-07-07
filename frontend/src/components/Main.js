9e91c25399121ffbf63a1c7670e19a6f193af3f0
Nadia Kotegova <nadinekoteg@gmail.com>
2021-07-07 12:18:31 +0300

    fix spinner

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -8,49 +8,50 @@ export default function Main({ isLoading, cards, onEditAvatar, onEditProfile, on
 
   return (
     <main className="content">
-      <section className="profile">
-        <div className="profile__info">
-          <div className="profile__avatar" onClick={onEditAvatar}>
-            <img src={currentUser.avatar} className="avatar" alt="аватар" />
-          </div>
-          <div className="profile__text">
-            <div className="profile__name-wrapper">
-              <h1 className="profile__name">{currentUser.name}</h1>
-              <button
-                className="profile__btn-edit"
-                onClick={onEditProfile}
-                type="button"
-                aria-label="Edit"
-              ></button>
+      {isLoading ?
+      <Spinner /> :(
+      <>
+        <section className="profile">
+          <div className="profile__info">
+            <div className="profile__avatar" onClick={onEditAvatar}>
+              <img src={currentUser.avatar} className="avatar" alt="аватар" />
             </div>
-            <p className="profile__job">{currentUser.about}</p>
+            <div className="profile__text">
+              <div className="profile__name-wrapper">
+                <h1 className="profile__name">{currentUser.name}</h1>
+                <button
+                  className="profile__btn-edit"
+                  onClick={onEditProfile}
+                  type="button"
+                  aria-label="Edit"
+                ></button>
+              </div>
+              <p className="profile__job">{currentUser.about}</p>
+            </div>
           </div>
-        </div>
-        <button
-          className="profile__btn-add"
-          onClick={onAddPlace}
-          type="button"
-          aria-label="Add"
-        ></button>
-      </section>
-      <section className="elements">
-        {isLoading ?
-          <Spinner /> :(
-          <ul className="elements__list">
-            {cards.map((card) => (
-              <Card
-                key={card._id}
-                card={card}
-                onConfirm={onConfirm}
-                onCardClick={onCardClick}
-                onCardLike={onCardLike}
-                onCardDelete={onCardDelete}
-              />
-            ))}
-          </ul>
-           )
-        } 
-      </section>
+          <button
+            className="profile__btn-add"
+            onClick={onAddPlace}
+            type="button"
+            aria-label="Add"
+          ></button>
+        </section>
+        <section className="elements">
+            <ul className="elements__list">
+              {cards.map((card) => (
+                <Card
+                  key={card._id}
+                  card={card}
+                  onConfirm={onConfirm}
+                  onCardClick={onCardClick}
+                  onCardLike={onCardLike}
+                  onCardDelete={onCardDelete}
+                />
+              ))}
+            </ul>
+        </section>
+      </>
+      )}
     </main>
-  );
+  )
 }
